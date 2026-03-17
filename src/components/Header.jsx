1b10dd2f11ac6dc6d7c8dc811f65aea5c3d58974
kevin <contactohuayta@gmail.com>
2026-03-17 20:39:36 -0300

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navegation from "./navegation";
 
 const Header = () => {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import imgportf from '../assets/img.png';
 import { TypeAnimation } from 'react-type-animation';
-import { motion } from "motion/react"
 
 const Home = () => {
     return (
